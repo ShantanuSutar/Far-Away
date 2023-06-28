bb183262e97e2ce287ce53013e5506fd330237ae
Shantanu Sutar <shantanusutar17@gmail.com>
2023-06-28 14:14:39 +0530

    sorting the items with input, description and packed status

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -85,10 +85,29 @@ function Form({ handleAdd }) {
 }
 
 function PackingList({ items, handleDelete, handleToggle }) {
+  const [sort, setSort] = useState("input"); // Create a state for the sort value
+
+  let sortedItems = [...items]; // Create a copy of the items array
+
+  if (sort === "input")
+    sortedItems = items; // If the sort value is "input", do nothing
+  else if (sort === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  // If the sort value is "description", sort the items by description
+  //LocaleCompare is a string method that compares two strings and returns a number
+  //slice() is used to create a copy of the array, so we don't mutate the original one
+  //sort() is used to sort the array
+  else if (sort === "packed")
+    sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
+
+  // If the sort value is "packed", sort the items by packed status
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -97,6 +116,18 @@ function PackingList({ items, handleDelete, handleToggle }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sort} onChange={(e) => setSort(e.target.value)}>
+          <option value="input">sort by input order</option>
+          <option value="description">sort by description</option>
+          <option value="packed">sort by packed status</option>
+        </select>
+      </div>
+      {/* 
+        Create a select element with three options
+        When the value of the select changes, update the sort state
+       */}
     </div>
   ); // Map over the items and create an Item component for each one
 }
